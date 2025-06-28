77ab02d067b981c9b7eb9e37e7d21c3f804399b5
Shresth-11 <jaisshresth143@gmail.com>
2025-06-28 16:36:48 +0530

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+let age = 19;
+let isLoggedIn = false;
+let state = null;
+
+// alert("hello"); we are using nodejs, not browser
+
+//number => 2 to power 53
+//bigint
+// boolean => true/false
+// string => ""
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined);  // output => undefined
+console.log(typeof null); // output => object
