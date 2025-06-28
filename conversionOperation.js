5771bfc35c8f3a84402ae64c48e7ae21ab192ac2
Shresth-11 <jaisshresth143@gmail.com>
2025-06-28 16:58:03 +0530

    conversion of datatypes

---
+++ conversionOperation.js
@@ -0,0 +1,30 @@
+/*let score = "33abc";
+let s = null;
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+let valueInNumber1 = Number(s);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // output => NaN
+
+console.log(valueInNumber1); // output => 0  */
+
+/*
+"33" => 33
+"33abc" => NaN but the type is still Number
+true => 1; false => 0
+null => 0
+*/
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+
