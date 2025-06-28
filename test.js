58b2340f87252d350a8129aa8d4f807baf1e0dc4
Shresth-11 <jaisshresth143@gmail.com>
2025-06-28 16:36:32 +0530

    notes

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
 console.log("Shresth");
+const accountId = 45454;
+let accountEmail = "shresth@gmail.com";
+var accountPassword = "545454";
+accountCity = "Delhhi";
+let accountState;
+
+console.log(accountId);
+
+// Node.js is the engine used to run javascript and there 
+// are two options to run javascript NODE and DINO but to for
+// learning process we prefer node.js engine.
+
+/* var is not used now coz of the issue in block and functional scope. */
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+
