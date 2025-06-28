a1d8901960c940235e864e0cc20105d3f89bb273
Shresth-11 <jaisshresth143@gmail.com>
2025-06-28 22:00:42 +0530

    database imp for interview

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+// JavaScript is a dynamically typed language
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+console.log(id === anotherId); //false
+
+const bigNumber = 8796546531254879874564n;
+
+// Reference(Non primitive)
+
+// Array, Objects, functions
+
+const heroes = ["shaktiman", "doga", "Ironman"];
+let obj = {
+  name: "Shresth",
+  age: 22,
+};
+
+const myFunction = function () {
+  console.log("Hello world");
+};
+
+console.log(typeof outsideTemp); // object output
+console.log(typeof heroes); // object output
