05fea5038e2464ec585b03765b42a73d076b8d07
Shresth-11 <jaisshresth143@gmail.com>
2025-06-29 22:28:16 +0530

    update

---
+++ control_flow/three.js
@@ -0,0 +1,30 @@
+// const coding = ["js", "rb", "py", "java", "cpp", "python"];
+
+// const values = coding.forEach((item) => {
+//   console.log(item);
+//   return item; // foreach loop does not return any value
+// });
+
+// console.log(values);
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums = nums.filter(
+//   // filter returns values
+//   (num) => num > 4
+// );
+
+// const newNums = nums.filter((num) => {
+//   // NOTE :--------------------------------------------     agar ye {} scope open kr rhe toh return use krna hoga value milne k liye
+//   return num > 4;
+// });
+// console.log(newNums);
+
+// nums.forEach((num) => {
+//   if (num > 4) {
+//     nums.push(num);
+//   }
+// });
+// console.log(nums);
+
+
