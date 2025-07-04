d7f5036215b4a57af6c33e5df5d147c19650c5c8
Shresth-11 <jaisshresth143@gmail.com>
2025-07-04 13:20:05 +0530

    async

---
+++ Async/async.js
@@ -0,0 +1,26 @@
+/* Javascript is synchronous and single threaded language  --------------- DEFAULT
+
+ Execution context -------------------------------------------->>>>>>>>>>>
+ executes one line of code at a time
+ each operation waits for the last one to complete before executing
+
+
+ In javascript two types of codes are written -------->>>
+
+ 1. Blocking code = block the flow the program. Read file sync.
+ 2.Non Blocking code = Does not block execution. Reads file async.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
